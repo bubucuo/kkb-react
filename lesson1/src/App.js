60ca57a9ef0473b2ad2cbdf0846745e05862ccc0
gaoshaoyun <276185876@qq.com>
2020-04-15 16:38:30 +0800

    init lesson1

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,16 +1,5 @@
-import React, {useState, useEffect} from "react";
-
-// import Button from "antd/es/button";
-// import "antd/dist/antd.css";
-
-import {Button} from "antd";
+import React from "react";
 
 export default function App(props) {
-  return (
-    <div>
-      <h3>App</h3>
-      <Button type="primary"> click</Button>
-      <a href="https://www.kaikeba.com/">kaiekba</a>
-    </div>
-  );
+  return <div>app</div>;
 }
