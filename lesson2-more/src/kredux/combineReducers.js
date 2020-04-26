9aa6432a65de42eea24f38f2ea1cffe3a582cd3b
gaoshaoyun <276185876@qq.com>
2020-04-26 16:24:12 +0800

    Update combineReducers.js

--- lesson2-more/src/kredux/combineReducers.js
+++ lesson2-more/src/kredux/combineReducers.js
@@ -1,17 +1,16 @@
 export default function combineReducers(reducers) {
-  // 返回reducer
   return function combination(state = {}, action) {
     let nextState = {};
     let hasChanged = false;
+
     for (let key in reducers) {
       const reducer = reducers[key];
       nextState[key] = reducer(state[key], action);
-
       hasChanged = nextState[key] !== state[key];
     }
 
     hasChanged =
-      hasChanged || Object.keys[state].length !== Object.keys[state].length;
+      hasChanged || Object.keys(reducers).length !== Object.keys(state).length;
 
     return hasChanged ? nextState : state;
   };
