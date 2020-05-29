69f52588a79df71b1eb475a0bc372b4a4dd0b634
gaoshaoyun <276185876@qq.com>
2020-05-29 13:17:42 +0800

    作业

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import ContextPage from "./pages/ContextPage";
-import HocPage from "./pages/HocPage";
-import AntdFormPage from "./pages/AntdFormPage";
-import MyRCFieldForm from "./pages/MyRCFieldForm";
+// import ContextPage from "./pages/ContextPage";
+// import HocPage from "./pages/HocPage";
+// import AntdFormPage from "./pages/AntdFormPage";
+// import MyRCFieldForm from "./pages/MyRCFieldForm";
+import MyRCForm from "./pages/MyRCForm";
 
 export default function App(props) {
   return (
@@ -10,7 +11,8 @@ export default function App(props) {
       {/* <ContextPage /> */}
       {/* <HocPage /> */}
       {/* <AntdFormPage /> */}
-      <MyRCFieldForm />
+      {/* <MyRCFieldForm /> */}
+      <MyRCForm />
     </div>
   );
 }
--- lesson1/src/components/Input.js
+++ lesson1/src/components/Input.js
@@ -4,10 +4,25 @@ const Input = props => {
   return <input {...props} />;
 };
 
-const CustomizeInput = ({value = "", ...props}) => (
-  <div style={{padding: 10}}>
-    <Input style={{outline: "none"}} value={value} {...props} />
-  </div>
-);
+// const CustomizeInput = ({value = "", ...props}) => (
+//   <div style={{padding: 10}}>
+//     <Input style={{outline: "none"}} value={value} {...props} />
+//   </div>
+// );
+
+class CustomizeInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const {value = "", ...otherProps} = this.props;
+    return (
+      <div style={{padding: 10}}>
+        <Input style={{outline: "none"}} value={value} {...otherProps} />
+      </div>
+    );
+  }
+}
 
 export default CustomizeInput;
