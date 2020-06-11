3cb79e1892917312d263ccccf0bcd2b5f0fc1251
gaoshaoyun <276185876@qq.com>
2020-06-11 15:26:44 +0800

    save lesson6 antd pro

--- lesson6-antd-pro/src/locales/zh-CN/menu.ts
+++ lesson6-antd-pro/src/locales/zh-CN/menu.ts
@@ -5,6 +5,7 @@ export default {
   'menu.admin': '管理页',
   'menu.admin.sub-page': '二级管理页',
   'menu.login': '登录',
+  'menu.more': '更多',
   'menu.register': '注册',
   'menu.register.result': '注册结果',
   'menu.dashboard': 'Dashboard',
