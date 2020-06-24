771e89bd258b417db34098001e4160e0fb74e7df
gaoshaoyun <276185876@qq.com>
2020-06-24 22:37:50 +0800

    ls1 save

--- lesson1/src/components/my-rc-field-form/Form.js
+++ lesson1/src/components/my-rc-field-form/Form.js
@@ -1,9 +1,22 @@
 import React from "react";
+import useForm from "./useForm";
+import FieldContext from "./FieldContext";
 
-export default function Form(props) {
+export default function Form({form, children, onFinish, onFinishFailed}) {
+  const [formInstance] = useForm(form);
+  formInstance.setCallback({
+    onFinish,
+    onFinishFailed
+  });
   return (
-    <div>
-      <h3>Form</h3>
-    </div>
+    <form
+      onSubmit={e => {
+        e.preventDefault();
+        formInstance.submit();
+      }}>
+      <FieldContext.Provider value={formInstance}>
+        {children}
+      </FieldContext.Provider>
+    </form>
   );
 }
