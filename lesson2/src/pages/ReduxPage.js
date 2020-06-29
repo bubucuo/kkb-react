df6278970a5537d0c7cfdfe43e7a9a02179bd658
gaoshaoyun <276185876@qq.com>
2020-06-29 16:39:41 +0800

    init les 2

---
+++ lesson2/src/pages/ReduxPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class ReduxPage extends Component {
+  render() {
+    return (
+      <div>
+        <h3>ReduxPage</h3>
+      </div>
+    );
+  }
+}
