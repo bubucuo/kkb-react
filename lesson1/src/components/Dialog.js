b369c1587f0248f344073881cfbd0722c25ab3d8
gaoshaoyun <276185876@qq.com>
2020-06-29 16:52:30 +0800

    save ls 1  portal

--- lesson1/src/components/Dialog.js
+++ lesson1/src/components/Dialog.js
@@ -4,23 +4,13 @@ import {createPortal} from "react-dom";
 export default class Dialog extends Component {
   constructor(props) {
     super(props);
-    const doc = window.document;
-    this.node = doc.createElement("div");
-    doc.body.appendChild(this.node);
-  }
-
-  componentWillUnmount() {
-    if (this.node) {
-      window.document.body.removeChild(this.node);
-    }
   }
 
   render() {
-    return createPortal(
+    return (
       <div className="dialog">
         <h3>Dialog</h3>
-      </div>,
-      this.node
+      </div>
     );
   }
 }
