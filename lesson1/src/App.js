789af582ea9cd361664e5fbfbc58a0a81fda593c
gaoshaoyun <276185876@qq.com>
2020-06-29 14:03:46 +0800

    lesson1 作业 save

--- lesson1/src/App.js
+++ lesson1/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import ContextPage from "./pages/ContextPage";
 import AntdFormPage from "./pages/AntdFormPage";
 import MyRCFieldForm from "./pages/MyRCFieldForm";
+import MyRCForm from "./pages/MyRCForm";
 
 export default function App(props) {
   return (
     <div>
       {/* <ContextPage /> */}
       {/* <AntdFormPage /> */}
-      <MyRCFieldForm />
+      {/* <MyRCFieldForm /> */}
+      <MyRCForm />
     </div>
   );
 }
