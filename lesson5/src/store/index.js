030d8033ca0686a8cb4830bc7af4b8512fd3416c
gaoshaoyun <276185876@qq.com>
2020-07-06 20:26:24 +0800

    init

--- lesson5/src/store/index.js
+++ lesson5/src/store/index.js
@@ -1,7 +1,5 @@
 import {loginReducer} from "./loginReducer";
-import {createStore, combineReducers, applyMiddleware} from "redux";
-import thunk from "redux-thunk";
-import createSagaMiddleware from "redux-saga";
+import {createStore, combineReducers} from "redux";
 
 const store = createStore(combineReducers({user: loginReducer}));
 
