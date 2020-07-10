1338ccdd8cb9567917342ae7c16a122097139591
gaoshaoyun <276185876@qq.com>
2020-07-10 10:58:15 +0800

    ls6 作业与pro扩展 save

---
+++ lesson6-antd-pro/config/defaultSettings.ts
@@ -0,0 +1,26 @@
+import { Settings as ProSettings } from '@ant-design/pro-layout';
+
+type DefaultSettings = ProSettings & {
+  pwa: boolean;
+};
+
+const proSettings: DefaultSettings = {
+  navTheme: 'dark',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: 'Ant Design Pro',
+  pwa: false,
+  iconfontUrl: '',
+};
+
+export type { DefaultSettings };
+
+export default proSettings;
