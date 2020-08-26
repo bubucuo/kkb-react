d25f39fe00ed5d1e91a5e02785e39974c061220d
gaoshaoyun <276185876@qq.com>
2020-08-26 20:05:18 +0800

    reset master

--- lesson1/src/Context.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ThemeContext = React.createContext({themeColor: "pink"});
-export const ThemeProvider = ThemeContext.Provider;
-export const ThemeConsumer = ThemeContext.Consumer;
-
-export const UserContext = React.createContext();
-export const UserProvider = UserContext.Provider;
-export const UserConsumer = UserContext.Consumer;
