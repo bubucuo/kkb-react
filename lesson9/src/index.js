260337f2b1128b174e93a95d20da5e76cdc59ca3
gaoshaoyun <276185876@qq.com>
2020-10-20 20:16:43 +0800

    init ls9

---
+++ lesson9/src/index.js
@@ -0,0 +1,60 @@
+// import React, {Component} from "react";
+// import ReactDOM from "react-dom";
+
+import React from "./kreact/";
+import ReactDOM from "./kreact/react-dom";
+import Component from "./kreact/Component";
+
+import "./index.css";
+
+class ClassComponent extends Component {
+  static defaultProps = {
+    color: "pink"
+  };
+  render() {
+    return (
+      <div className="border">
+        <h3>ClassComponent</h3>
+        <p className={this.props.color}>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+function FunctionComponent(props) {
+  return (
+    <div className="border">
+      FunctionComponent
+      <p>{props.name}</p>
+    </div>
+  );
+}
+
+const jsx = (
+  <div className="border">
+    <p>全栈</p>
+    <a href="https://www.kaikeba.com/">开课吧</a>
+    <FunctionComponent name="函数组件" />
+    <ClassComponent name="类组件" color="red" />
+    {/* {[1, 2, 3].map(item => (
+      < >
+        <p>1</p>
+        <p>2</p>
+      </>
+    ))} */}
+
+    {/* <>
+      <p>1</p>
+      <p>2</p>
+    </> */}
+  </div>
+);
+
+// 这里的jsx最终会被编译为createElement(),
+ReactDOM.render(jsx, document.getElementById("root"));
+
+console.log("vserion", React.version); //sy-log
+// 原生标签节点
+// 文本节点
+// 函数组件
+// 类组件
