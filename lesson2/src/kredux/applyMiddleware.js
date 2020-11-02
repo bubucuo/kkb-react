4b23fd721fdbe09265a79227b73bbc947a79dc34
gaoshaoyun <276185876@qq.com>
2020-11-02 22:24:29 +0800

    save ls2

--- lesson2/src/kredux/applyMiddleware.js
+++ lesson2/src/kredux/applyMiddleware.js
@@ -1,3 +1,4 @@
+// *
 export default function applyMiddleware(...middlewares) {
   return createStore => reducer => {
     const store = createStore(reducer);
@@ -5,10 +6,29 @@ export default function applyMiddleware(...middlewares) {
     let dispatch = store.dispatch;
     // todo 加强store.dispatch
 
+    const midApi = {
+      getState: store.getState,
+      dispatch: action => dispatch(action)
+    };
+
+    const middlewareChain = middlewares.map(middleware => middleware(midApi));
+    // 重新赋值一个函数
+    dispatch = compose(...middlewareChain)(store.dispatch);
+
     // 返回加强版，加强store.dispatch
     return {
       ...store,
-      ...dispatch
+      dispatch
     };
   };
+}
+
+function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+  return funcs.reduce((a, b) => (...args) => a(b(...args)));
 }
--- lesson2/src/store/index.js
+++ lesson2/src/store/index.js
@@ -1,7 +1,9 @@
-// import {createStore, applyMiddleware} from "redux";
-import {createStore, applyMiddleware} from "../kredux/";
-import thunk from "redux-thunk"; // 异步解决方案
-import logger from "redux-logger"; // 打印日志
+import {createStore, applyMiddleware, combineReducers} from "redux";
+// import {createStore, applyMiddleware} from "../kredux/";
+// import thunk from "redux-thunk"; // 异步解决方案
+// import logger from "redux-logger"; // 打印日志
+// import promise from "redux-promise"; // 处理promise
+import isPromise from "is-promise";
 
 // 定义修改store state的规则
 function countReducer(state = 0, action) {
@@ -15,6 +17,54 @@ function countReducer(state = 0, action) {
   }
 }
 
-const store = createStore(countReducer, applyMiddleware(thunk, logger));
+// logger要作为applyMiddleware的最后一个参数，不然不能保证action是plain object
+const store = createStore(
+  // countReducer,
+  combineReducers({count: countReducer}),
+  applyMiddleware(promise, thunk, logger)
+);
 
 export default store;
+
+// function a() {
+//   return {
+//     return {}
+//   }
+// }
+
+function logger({dispatch, getState}) {
+  return next => action => {
+    console.log("++++++++++++++++++++++++++"); //sy-log
+
+    console.log(action.type + "执行了！！！"); //sy-log
+
+    const prevState = getState();
+    console.log("prev state", prevState); //sy-log
+
+    // todo
+    const returnValue = next(action);
+
+    const nextState = getState();
+    console.log("prev state", nextState); //sy-log
+
+    console.log("++++++++++++++++++++++++++"); //sy-log
+
+    return returnValue;
+  };
+}
+
+function thunk({dispatch, getState}) {
+  return next => action => {
+    // action 数据类型是？对象 | 函数
+    if (typeof action === "function") {
+      return action(dispatch, getState);
+    }
+    return next(action);
+  };
+}
+
+function promise({dispatch}) {
+  return next => action => {
+    return isPromise(action) ? action.then(dispatch) : next(action);
+  };
+}
