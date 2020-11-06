d25009ab19b58b9f583ef9222abfeeb5c2e09700
gaoshaoyun <276185876@qq.com>
2020-11-06 23:18:49 +0800

    可以去Router中注释掉改变this.unlisten,然后点击这里的count的参数值 ，查看卸载的组件的setState的错误*
     可以去Router中注释掉改变this.unlisten,然后点击这里的count的参数值 ，查看卸载的组件的setState的错误

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -1,30 +1,30 @@
 import React, {useState} from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect,
-  useHistory,
-  useLocation,
-  useRouteMatch,
-  useParams,
-  withRouter,
-  Prompt
-} from "react-router-dom";
-
 // import {
 //   BrowserRouter as Router,
 //   Route,
 //   Link,
 //   Switch,
+//   Redirect,
 //   useHistory,
 //   useLocation,
 //   useRouteMatch,
 //   useParams,
-//   withRouter
-//   // Redirect
-// } from "./k-react-router-dom/";
+//   withRouter,
+//   Prompt
+// } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  useHistory,
+  useLocation,
+  useRouteMatch,
+  useParams,
+  withRouter
+  // Redirect
+} from "./k-react-router-dom/";
 
 import HomePage from "./pages/HomePage";
 import UserPage from "./pages/UserPage";
@@ -32,7 +32,7 @@ import LoginPage from "./pages/LoginPage";
 import _404Page from "./pages/_404Page";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
   const id = 123;
   return (
     <div className="App">
@@ -42,33 +42,37 @@ function App() {
         }}>
         add: {count}
       </button>
-      <Router>
-        <Link to="/">首页</Link>
-        <Link to="/user">用户中心</Link>
-        <Link to="/login">登录</Link>
-        <Link to="/product/123">商品</Link>
+      {/* // ! 可以去Router中注释掉改变this.unlisten,然后点击这里的count的参数值 ，查看卸载的组件的setState的错误*/}
+      <a href="#/a">a</a>
+      {count % 2 && (
+        <Router>
+          <Link to="/">首页</Link>
+          <Link to="/user">用户中心</Link>
+          <Link to="/login">登录</Link>
+          <Link to="/product/123">商品</Link>
 
-        <Switch>
-          <Route
-            path="/"
-            exact
-            //children={children}
-            component={HomePage}
-            // ! 下面写法影响性能
-            //component={() => <HomePage />}
+          <Switch>
+            <Route
+              path="/"
+              exact
+              //children={children}
+              component={HomePage}
+              // ! 下面写法影响性能
+              //component={() => <HomePage />}
 
-            // render={render}
-          />
-          <Route
-            path="/product/:id"
-            // component={Product}
-            render={() => <Product />}
-          />
-          <Route path="/user" component={UserPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route component={_404Page} />
-        </Switch>
-      </Router>
+              // render={render}
+            />
+            <Route
+              path="/product/:id"
+              // component={Product}
+              render={() => <Product />}
+            />
+            <Route path="/user" component={UserPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route component={_404Page} />
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
@@ -79,7 +83,7 @@ export default App;
 class Product extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {confirm: true};
+    this.state = {confirm: false};
   }
   render() {
     const {match} = this.props;
@@ -91,13 +95,13 @@ class Product extends React.Component {
         Product:{id}
         <Link to={url + "/detail"}>详情</Link>
         <Route path={url + "/detail"} component={Detail} />
-        <Prompt
+        {/* <Prompt
           when={this.state.confirm}
           // message="Are you sure you want to leave?"
           message={location => {
             return "Are you sure you want to leave-fun";
           }}
-        />
+        /> */}
       </div>
     );
   }
