46be2492a84d3120d5f2323e113915a1f7ddc97f
gaoshaoyun <276185876@qq.com>
2020-11-09 20:22:34 +0800

    Update index.js

--- lesson5/src/store/index.js
+++ lesson5/src/store/index.js
@@ -1,6 +1,5 @@
 import {loginReducer} from "./loginReducer";
 import {createStore, combineReducers} from "redux";
-import loginSaga from "../action/loginSaga";
 
 const store = createStore(combineReducers({user: loginReducer}));
 
