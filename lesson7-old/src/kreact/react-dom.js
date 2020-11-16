07a00161979d590946c55386111a96f9cbe1642f
gaoshaoyun <276185876@qq.com>
2020-11-16 15:18:15 +0800

    save ls 7 old

--- lesson7-old/src/kreact/react-dom.js
+++ lesson7-old/src/kreact/react-dom.js
@@ -1,52 +1,74 @@
-import {TEXT} from "./const";
-
-// ! vnode、vvnode 虚拟dom节点
-// ! node 真实dom节点
+// vnode 虚拟dom节点
+// node 真实dom节点
 
 function render(vnode, container) {
-  console.log("vnode--render", vnode); //sy-log
-  // vnode->node
+  console.log("vnode", vnode); //sy-log
+  //step1: vnode->node
+
   const node = createNode(vnode);
-  // 把node插入container
+
+  //step2: container.appendChild(node)
+
   container.appendChild(node);
 }
 
-// 生成真实dom节点
 function createNode(vnode) {
   let node = null;
+
   const {type, props} = vnode;
 
-  if (type === TEXT) {
-    //创建文本节点
+  if (type === "TEXT") {
+    // 文本
     node = document.createTextNode("");
   } else if (typeof type === "string") {
-    // 证明是个html标签节点， 比如div、span
+    // 原生标签
     node = document.createElement(type);
   } else if (typeof type === "function") {
-    // 类组件 或者函数组件
-
     node = type.isReactComponent
       ? updateClassComponent(vnode)
       : updateFunctionComponent(vnode);
   } else {
+    // 源码中现在没有用createDocumentFragment，而是用的fiber节点
     node = document.createDocumentFragment();
   }
 
+  // todo step1 处理子节点
   reconcileChildren(props.children, node);
+
+  // todo 处理自身属性
   updateNode(node, props);
   return node;
 }
 
-// 遍历下子节点
+// 处理函数组件
+function updateFunctionComponent(vnode) {
+  const {type, props} = vnode;
+
+  const vvnode = type(props);
+  // vvnode->node
+  const node = createNode(vvnode);
+  return node;
+}
+
+// 处理类组件
+function updateClassComponent(vnode) {
+  const {type, props} = vnode;
+  const instance = new type(props);
+  const vvnode = instance.render();
+  // vvnode->node
+  const node = createNode(vvnode);
+  return node;
+}
+
 function reconcileChildren(children, node) {
-  for (let i = 0; i < children.length; i++) {
-    let child = children[i];
-    // child是vnode，那需要把vnode->真实dom节点，然后插入父节点node中
+  for (let index = 0; index < children.length; index++) {
+    const child = children[index];
+    // vnode->dom节点， 插入到node父节点中
     render(child, node);
   }
 }
 
-// 添加节点属性
+// 处理属性
 function updateNode(node, nextVal) {
   Object.keys(nextVal)
     .filter(k => k !== "children")
@@ -55,19 +77,4 @@ function updateNode(node, nextVal) {
     });
 }
 
-function updateClassComponent(vnode) {
-  const {type, props} = vnode;
-  const cmp = new type(props);
-  const vvnode = cmp.render();
-  // 返回真实dom节点
-  const node = createNode(vvnode);
-  return node;
-}
-
-function updateFunctionComponent(vnode) {
-  const {type, props} = vnode;
-  const vvnode = type(props);
-  const node = createNode(vvnode);
-  return node;
-}
 export default {render};
