084f55e90edf14494a127a88d75147c9460043b5
zhidl <zhidali@julive.com>
2020-11-24 10:18:13 +0800

    1

---
+++ lesson9/src/Memo.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { useState, useMemo } from 'react';
+export default function UseMemoPage(props) {
+
+  
+	const [ count, setCount ] = useState(0);
+  const [ value, setValue ] = useState('');
+  
+
+	// const expensive = () => {
+		
+		
+  // };
+  
+  // 暗号：椰子
+  const expensive = useMemo(() => {
+    console.log('compute');
+    let sum = 0;
+		for (let i = 0; i < count; i++) {
+			sum += i;
+		}
+		return sum;
+  }, [count])
+	return (
+		<div>
+			<h3>UseMemoPage</h3>
+			<p>expensive:{expensive}</p>
+			<p>{count}</p>
+			<button onClick={() => setCount(count + 1)}>add</button>
+			<input value={value} onChange={(event) => setValue(event.target.value)} />
+		</div>
+	);
+}
---
+++ lesson9/src/index.jsx
@@ -0,0 +1,69 @@
+// import React, {Component, useState} from "react";
+// import ReactDOM from "react-dom";
+// import ReactDOM, {useState} from "./kreact/react-dom";
+// import Component from "./kreact/Component";
+
+// import "./index.css";
+
+// class ClassComponent extends Component {
+//   render() {
+//     return (
+//       <div className="border">
+//         <p>{this.props.name}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// function FunctionComponent(props) {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <div className="border">
+//       <button
+//         onClick={() => {
+//           console.log("count", count); //sy-log
+//           setCount(count + 1);
+//         }}>
+//         {count + ""}
+//       </button>
+//       {count % 2 ? <p>{props.name}</p> : <span>omg</span>}
+//     </div>
+//   );
+// }
+
+// const jsx = (
+//   <div className="border">
+//     <p>全栈</p>
+//     <a href="https://www.kaikeba.com/">开课吧</a>
+//     <FunctionComponent name="函数组件" />
+//     <ClassComponent name="类组件" />
+//   </div>
+// );
+
+// ReactDOM.render(jsx, document.getElementById("root"));
+
+// 文本标签
+// 原生标签
+// 函数组件
+// 类组件
+
+
+
+
+
+import React from 'react';
+// 负责渲染实际dom  vdom -> dom转换
+import ReactDOM from 'react-dom';
+
+import Memo from './Memo';
+
+import Callback from './Callback';
+ReactDOM.render(
+  (
+    <div className="app">
+      {/* <Memo /> */}
+      <Callback />
+    </div>
+  ),
+  document.getElementById("root")
+)
