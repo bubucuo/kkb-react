b7cb0d57e3b8636169b7737d7c6403b5d3a51261
gaoshaoyun <276185876@qq.com>
2020-11-28 20:17:04 +0800

    init ls2

---
+++ lesson2/src/store/index.js
@@ -0,0 +1,17 @@
+import {createStore} from "redux";
+
+// 定义修改规则
+function countReducer(state = 0, action) {
+  switch (action.type) {
+    case "ADD":
+      return state + 1;
+    case "MINUS":
+      return state - action.payload || 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(countReducer);
+
+export default store;
