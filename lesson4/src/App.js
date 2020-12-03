0ab516383824d266cdc0e0cb98cd2c932f78ea8e
gaoshaoyun <276185876@qq.com>
2020-12-03 21:37:39 +0800

    Update App.js

--- lesson4/src/App.js
+++ lesson4/src/App.js
@@ -1,30 +1,30 @@
 import React, {useState} from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-  Redirect,
-  useHistory,
-  useLocation,
-  useRouteMatch,
-  useParams,
-  withRouter,
-  Prompt
-} from "react-router-dom";
-
 // import {
 //   BrowserRouter as Router,
 //   Route,
 //   Link,
 //   Switch,
+//   Redirect,
 //   useHistory,
 //   useLocation,
 //   useRouteMatch,
 //   useParams,
-//   withRouter
-//   // Redirect
-// } from "./k-react-router-dom/";
+//   withRouter,
+//   Prompt
+// } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+  // Switch,
+  // useHistory,
+  // useLocation,
+  // useRouteMatch,
+  // useParams,
+  // withRouter
+  // Redirect
+} from "./k-react-router-dom/";
 
 import HomePage from "./pages/HomePage";
 import UserPage from "./pages/UserPage";
@@ -32,24 +32,69 @@ import LoginPage from "./pages/LoginPage";
 import _404Page from "./pages/_404Page";
 
 function App() {
+  const [count, setCount] = useState(1);
+
   return (
     <div className="App">
-      <Router>
-        <Link to="/">首页</Link>
-        <Link to="/user">用户中心</Link>
-        <Link to="/login">登录</Link>
-        <Link to="/product/123">商品</Link>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      {count < 3 && (
+        <Router>
+          <Link to="/">首页</Link>
+          <Link to="/user">用户中心</Link>
+          <Link to="/login">登录</Link>
+          <Link to="/product/123">商品</Link>
 
-        <Switch>
-          <Route path="/" exact component={HomePage} />
+          {/* <Switch> */}
+          <Route
+            path="/"
+            exact
+            //children={children}
+            component={HomePage}
+            // ! 这个写法影响性能
+            //component={() => <HomePage />}
+            //render={() => <HomePage />}
+            //render={render}
+          />
           <Route path="/user" component={UserPage} />
           <Route path="/login" component={LoginPage} />
+          {/* <Route path="/product/:id" component={Product} /> */}
           <Route component={_404Page} />
-        </Switch>
-      </Router>
-      )
+          {/* </Switch> */}
+        </Router>
+      )}
     </div>
   );
 }
 
 export default App;
+
+function Product({match}) {
+  console.log("match", match); //sy-log
+  const {params, url} = match;
+  const {id} = params;
+  return (
+    <div>
+      <h1>Search-{id}</h1>
+      <Link to={url + "/detail"}>详情</Link>
+      <Route path={url + "/detail"} component={Detail} />
+    </div>
+  );
+}
+
+function Detail({match}) {
+  return (
+    <div>
+      <h1>detail</h1>
+    </div>
+  );
+}
+
+function children(props) {
+  console.log("children props", props); //sy-log
+  return <div>children</div>;
+}
+
+function render(props) {
+  console.log("render props", props); //sy-log
+  return <div>render</div>;
+}
