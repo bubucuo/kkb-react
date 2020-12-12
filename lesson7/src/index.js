57095ac75f3ecd7fc256851bc9b902830ca2f769
gaoshaoyun <276185876@qq.com>
2020-12-12 20:18:28 +0800

    init ls7

---
+++ lesson7/src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const jsx = (
+  <div className="border">
+    <h1>全栈</h1>
+    <a href="https://www.kaikeba.com/">kkb</a>
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
