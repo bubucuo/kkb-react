d716165d396abe741704dec0de63da1a7d2f89af
gaoshaoyun <276185876@qq.com>
2020-12-17 15:25:22 +0800

    save ls9

---
+++ lesson9/src/kreact/Component.js
@@ -0,0 +1,4 @@
+export default function Component(props) {
+  this.props = props;
+}
+Component.prototype.isReactComponent = {};
---
+++ lesson9/src/kreact/react-dom.js
@@ -0,0 +1,211 @@
+// vnode 虚拟dom节点
+// node  真实dom节点
+
+// fiber root,wip work in progress
+let wipRoot = null; // fiber | null
+
+function render(vnode, container) {
+  wipRoot = {
+    type: "div",
+    props: {children: {...vnode}},
+    stateNode: container
+  };
+  nextUnitOfWork = wipRoot;
+}
+
+function isStringOrNumber(sth) {
+  return typeof sth === "string" || typeof sth === "number";
+}
+// 根据原生标签的fiber生成dom节点
+function createNode(workInProgress) {
+  const {type, props} = workInProgress;
+  const node = document.createElement(type);
+  updateNode(node, props);
+  return node;
+}
+
+// 更新原生标签的，即根据原生标签的vnode生成node
+function updateHostComponent(workInProgress) {
+  if (!workInProgress.stateNode) {
+    workInProgress.stateNode = createNode(workInProgress);
+  }
+  // todo 协调子节点
+  reconcileChildren(workInProgress, workInProgress.props.children);
+}
+
+// 更新原生标签的属性，如className、href、id、（style、事件）等
+function updateNode(node, nextVal) {
+  Object.keys(nextVal)
+    // .filter(k => k !== "children")
+    .forEach(k => {
+      if (k === "children") {
+        // 有可能是文本
+        if (isStringOrNumber(nextVal[k])) {
+          node.textContent = nextVal[k] + "";
+        }
+      } else {
+        node[k] = nextVal[k];
+      }
+    });
+}
+
+// 函数组件 执行函数
+// 协调子节点
+function updateFunctionComponent(workInProgress) {
+  const {type, props} = workInProgress;
+  const child = type(props);
+  reconcileChildren(workInProgress, child);
+}
+
+// 类组件
+// 先实例化 再执行render函数
+function updateClassComponent(workInProgress) {
+  const {type, props} = workInProgress;
+  const instance = new type(props);
+  const child = instance.render();
+  reconcileChildren(workInProgress, child);
+}
+
+// 文本节点
+function updateTextComponent(vnode) {
+  const node = document.createTextNode(vnode);
+  return node;
+}
+
+// 实现Fragment
+function updateFragmentComponent(workInProgress) {
+  reconcileChildren(workInProgress, workInProgress.props.children);
+}
+
+// 遍历子节点
+function reconcileChildren(workInProgress, children) {
+  if (isStringOrNumber(children)) {
+    return;
+  }
+
+  const newChildren = Array.isArray(children) ? children : [children];
+
+  // 记录上一个fiber
+  let previousNewFiber = null;
+  for (let i = 0; i < newChildren.length; i++) {
+    let child = newChildren[i];
+    // 构建fiber结构
+    let newFiber = {
+      type: child.type,
+      props: {...child.props},
+      child: null, //fiber | nunll
+      sibling: null, // fiber | null
+      return: workInProgress, //fiber
+      stateNode: null // 如果是原生标签，这里dom节点
+    };
+
+    if (i === 0) {
+      // workInProgress的第一个子fiber
+      workInProgress.child = newFiber;
+    } else {
+      previousNewFiber.sibling = newFiber;
+    }
+    previousNewFiber = newFiber;
+  }
+}
+
+//* fiber属性
+// type 类型
+// key 标记当前层级下的唯一性
+// props 属性
+// ! child 第一个子节点
+// ! sibling 下一个兄弟节点
+// ! return 父节点（暂时）
+// stateNode 原生标签的就是dom节点，Fragment和函数组件的没有，类组件的是实例，
+// index 是个数字，从0开始，标记当前层级下的位置
+// *
+
+// 下一个需要更新的任务 fiber
+let nextUnitOfWork = null;
+// work in progress 正在工作中的fiber任务
+
+function performUnitOfWork(workInProgress) {
+  // * step1: 更新fiber任务
+  // todo
+  // 根据fiber任务类型不同进行更新
+  const {type} = workInProgress;
+
+  if (typeof type === "string") {
+    // 原生标签节点
+    updateHostComponent(workInProgress);
+  } else if (typeof type === "function") {
+    type.prototype.isReactComponent
+      ? updateClassComponent(workInProgress)
+      : updateFunctionComponent(workInProgress);
+  } else {
+    updateFragmentComponent(workInProgress);
+  }
+
+  // * step2: 返回下一个要更新的fiber任务，顺序参考王朝的故事
+  // ! 规则： 先子节点，
+  if (workInProgress.child) {
+    return workInProgress.child;
+  }
+  // ! 规则： 再兄弟节点或者叔叔节点或者爷爷弟弟节点等等
+  let nextFiber = workInProgress;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.return;
+  }
+}
+
+function workLoop(IdleDeadline) {
+  // 剩余空闲时间
+  while (nextUnitOfWork && IdleDeadline.timeRemaining() > 1) {
+    // 更新fiber任务
+    // 返回下一个要更新的fiber任务
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+  }
+
+  requestIdleCallback(workLoop);
+  // 任务更新完啦，
+  // todo 把fiber节点更新到dom当中
+  if (!nextUnitOfWork && wipRoot) {
+    // 可以提交任务，更新去了 就是可以同步虚拟dom节点到dom节点了
+    commitRoot();
+  }
+}
+
+requestIdleCallback(workLoop);
+
+// 提交
+function commitRoot() {
+  commitWorker(wipRoot.child);
+  wipRoot = null;
+}
+
+function commitWorker(workInProgress) {
+  if (!workInProgress) {
+    return;
+  }
+
+  // step1 : 提交自己
+  // 父dom节点的fiber
+  let parentNodeFiber = workInProgress.return;
+  // ? 一个fiber节点一定有dom节点吗，比如Fragment、Provider、Consumer等
+  while (!parentNodeFiber.stateNode) {
+    parentNodeFiber = parentNodeFiber.return;
+  }
+  // parentNode是父dom节点
+  let parentNode = parentNodeFiber.stateNode;
+
+  // 新增插入
+  if (workInProgress.stateNode) {
+    // 插入
+    parentNode.appendChild(workInProgress.stateNode);
+  }
+
+  // step2 : 提交子节点
+  commitWorker(workInProgress.child);
+  // step3 : 提交兄弟
+  commitWorker(workInProgress.sibling);
+}
+
+export default {render};
