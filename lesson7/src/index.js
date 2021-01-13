0c667ee29c87e3fddac2cfbaf230a471fc7d03dc
gaoshaoyun <276185876@qq.com>
2021-01-13 22:33:58 +0800

    save ls7

--- lesson7/src/index.js
+++ lesson7/src/index.js
@@ -1,14 +1,55 @@
 // import React from "react";
+// import React, {Component} from "react";
 // import ReactDOM from "react-dom";
 
 import ReactDOM from "./kreact/react-dom";
+import Component from "./kreact/Component";
 
 import "./index.css";
 
+function FunctionComponent(props) {
+  return (
+    <div className="border">
+      <p>{props.name}</p>
+    </div>
+  );
+}
+
+class ClassComponent extends Component {
+  render() {
+    return (
+      <div className="border">
+        <p>{this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+// function FragmentComponent(props) {
+//   return [1, 2, 3].map((item) => (
+//     <React.Fragment key={item}>
+//       <h1>111</h1>
+//       <h1>222</h1>
+//     </React.Fragment>
+//   ));
+// }
+
+function FragmentComponent(props) {
+  return (
+    <>
+      <h1>111</h1>
+      <h1>222</h1>
+    </>
+  );
+}
+
 const jsx = (
   <div className="border">
-    <h1>全栈</h1>
+    <h1>全栈123</h1>
     <a href="https://www.kaikeba.com/">kkb</a>
+    <FunctionComponent name="函数组件" />
+    <ClassComponent name="类组件" />
+    <FragmentComponent />
   </div>
 );
 
@@ -18,3 +59,6 @@ ReactDOM.render(jsx, document.getElementById("root"));
 
 // 原生标签节点
 // 文本节点
+// 函数组件
+// 类组件
+// Fragment
--- lesson7/src/kreact/react-dom.js
+++ lesson7/src/kreact/react-dom.js
@@ -14,6 +14,7 @@ function isStringOrNumber(sth) {
   return typeof sth === "string" || typeof sth === "number";
 }
 
+// vnode->node
 function createNode(vnode) {
   let node;
 
@@ -22,26 +23,74 @@ function createNode(vnode) {
   const {type} = vnode;
 
   if (typeof type === "string") {
+    // console.log("原生标签", vnode); //sy-log
     // 原生标签节点
     node = updateHostComponent(vnode);
   } else if (isStringOrNumber(vnode)) {
+    // console.log("文本", vnode); //sy-log
     // 文本 (字符串或者是数字)
     node = updateTextComponent(vnode);
+  } else if (typeof type === "function") {
+    // 再判断是函数组件还是类组件
+    node = type.prototype.isReactComponent
+      ? updateClassComponent(vnode)
+      : updateFunctionComponent(vnode);
   }
 
   return node;
 }
 
+function updateNode(node, nextVal) {
+  Object.keys(nextVal)
+    .filter((k) => k !== "children")
+    .forEach((k) => {
+      node[k] = nextVal[k];
+    });
+}
+
 // 原生标签节点
 function updateHostComponent(vnode) {
-  const {type} = vnode;
+  const {type, props} = vnode;
   const node = document.createElement(type);
+  updateNode(node, props);
+  reconcileChildren(node, props.children);
   return node;
 }
 
 function updateTextComponent(vnode) {
   const node = document.createTextNode(vnode + "");
   return node;
+}
+
+// 函数组件
+function updateFunctionComponent(vnode) {
+  const {type, props} = vnode;
+  const child = type(props);
+  // vnode->node
+  const node = createNode(child);
+  return node;
+}
+
+// 类组件
+function updateClassComponent(vnode) {
+  const {type, props} = vnode;
+  const instance = new type(props);
+  const child = instance.render();
+
+  // vnode->node
+  const node = createNode(child);
+  return node;
+}
+
+// 和现在的React17很大不同，下节课说
+function reconcileChildren(parentNode, children) {
+  const newChildren = Array.isArray(children) ? children : [children];
+  for (let i = 0; i < newChildren.length; i++) {
+    let child = newChildren[i];
+    // 遍历vnode
+    // vnode->node, parentNode.appendChild(node)
+    render(child, parentNode);
+  }
 }
 
 export default {render};
