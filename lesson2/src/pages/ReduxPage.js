97068702ff17cd81c21458dc661d075a6df4a1ab
gaoshaoyun <276185876@qq.com>
2021-01-28 20:30:07 +0800

    save ls2 init

---
+++ lesson2/src/pages/ReduxPage.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react";
+import store from "../store/";
+
+export default class ReduxPage extends Component {
+  componentDidMount() {
+    // store发生变化之后，执行subscribe的监听函数
+    store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
+
+  add = () => {
+    // 修改状态
+    store.dispatch({type: "ADD"});
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>ReduxPage</h3>
+        <p>{store.getState()}</p>
+        <button onClick={this.add}>add</button>
+        {/* <button onClick={this.asyAdd}>asyAdd</button> */}
+        {/* <button onClick={this.promiseMinus}>promiseMinus</button> */}
+      </div>
+    );
+  }
+}
